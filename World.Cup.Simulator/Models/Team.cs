23fbe13009d5bcfaa583437b9b23e3c27ff76767
jlmm9 <jlmm9@DESKTOP-RF120VJ>
2022-11-30 00:06:19 -0300

    Add project files.

---
+++ World.Cup.Simulator/Models/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace World.Cup.Simulator.Models
+{
+    public class Team
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+    }
+}
